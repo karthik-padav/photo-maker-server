6b38ad1ea8b2d1231b4828424312a81f1d989ae2
karthikpadav <karthikpadav@gmail.com>
2024-10-04 22:30:11 +0530

    code cleanup

--- routes/default.js
+++ routes/default.js
@@ -3,17 +3,6 @@ const router = express.Router();
 const axios = require("axios");
 
 router.route("/").get(async (req, res) => {
-  const SPACE_URL = "https://karthikpadav-bria-rmbg-clone.hf.space/run/predict";
-  try {
-    const response = await axios.get(SPACE_URL);
-    console.log(`Ping successful! Status code: ${response.status}`);
-  } catch (error) {
-    console.error(
-      `Error pinging the Hugging Face Space: ${
-        error.response ? error.response.data : error.message
-      }`
-    );
-  }
   res.status(200).json({ message: "We are up and running" });
 });
 
