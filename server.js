8c32014642d5afa326f4e5bdb61b76937ddb1403
karthikpadav <karthikpadav@gmail.com>
2024-10-05 11:29:00 +0530

    tweaks

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const errorHandler = require("./midleware/errorHandler");
-const connectDb = require("./config/dbConnection,js");
+// const connectDb = require("./config/dbConnection,js");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
 const bodyParser = require("body-parser");
 
-connectDb();
+// connectDb();
 const app = express();
 
 const port = process.env.PORT || 3000;
@@ -14,18 +14,18 @@ const port = process.env.PORT || 3000;
 app.use("/", require("./routes/default"));
 // const cron = require("node-cron");
 
-const Image = require("./routes/imagesRoutes");
-const Controler = require("./routes/controlerRoutes");
-const User = require("./routes/userRoutes");
+// const Image = require("./routes/imagesRoutes");
+// const Controler = require("./routes/controlerRoutes");
+// const User = require("./routes/userRoutes");
 
 app.use(
   bodyParser.json({ limit: "30mb", extended: true }),
   bodyParser.urlencoded({ limit: "30mb", extended: true }),
   errorHandler,
-  cors(),
-  Image,
-  Controler,
-  User
+  cors()
+  // Image,
+  // Controler,
+  // User
 );
 
 app.listen(port, () => {
